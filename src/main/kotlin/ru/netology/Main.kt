9f29a9c3f2aa1e52645cccd4b2f2a2aeac5b8221
OlmioGIT <olmihailoveg@gmail.com>
2022-03-29 14:38:37 +0300

    2.3

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,7 +1,6 @@
 package ru.netology
 
-import ru.netology.data.Comments
-import ru.netology.data.Post
+import ru.netology.data.*
 import ru.netology.service.WallService
 
 
@@ -18,14 +17,24 @@ fun main () {
         replyPostId = 1,
         friendsOnly = true,
         comments = Comments(1, true, true, true, true),
+        copyright = null,
+        likes = Likes(userLikes = true, canLike = true, canPublish = true, count = 4),
+        reposts = Reposts(id = 15, user_reposted = true),
+        views = 77,
+        postType = "",
+        postSource = "",
+        attachments = PhotoAttachments(url = "",id =12,ownerId = 15,albumId = 12, updateUserId = 121),
+        geo = null,
         signerId = 1,
+        copyHistory = "",
         canPin = true,
         canDelete = true,
         canEdit = true,
-        isPinned= true,
-        markedAsAds= true,
-        postponedId = 1
-
+        isPinned = true,
+        markedAsAds = true,
+        isFavorite = true,
+        donut = null,
+        postponedId = 1,
     )
 
     WallService.add(secondOrigin)
@@ -39,15 +48,26 @@ fun main () {
         text = "Stars is really beautiful",
         replyOwnerId = 1,
         replyPostId = 1,
-        friendsOnly = false,
+        friendsOnly = true,
         comments = Comments(1, true, true, true, true),
+        copyright = null,
+        likes = Likes(userLikes = true, canLike = true, canPublish = true, count = 4),
+        reposts = Reposts(id = 15, user_reposted = true),
+        views = 77,
+        postType = "",
+        postSource = "",
+        attachments = PhotoAttachments(url = "",id =12,ownerId = 15,albumId = 12, updateUserId = 121),
+        geo = null,
         signerId = 1,
-        canPin = false,
-        canDelete = false,
-        canEdit = false,
-        isPinned= false,
-        markedAsAds= false,
-        postponedId = 1
+        copyHistory = "",
+        canPin = true,
+        canDelete = true,
+        canEdit = true,
+        isPinned = true,
+        markedAsAds = true,
+        isFavorite = true,
+        donut = null,
+        postponedId = 1,
         )
 
     WallService.update(secondMaster)
--- src/main/kotlin/ru/netology/data/Post.kt
+++ src/main/kotlin/ru/netology/data/Post.kt
@@ -1,68 +1,108 @@
 package ru.netology.data
 
 data class Post(
-    val id: Long =0,
+    val id: Long = 0,
     val authorId: Int,
     val fromId: Int,
     val createdBy: Int,
     val date: Int,
-    val text: String,
-    val replyOwnerId: Int,
-    val replyPostId: Int,
+    val text: String?,
+    val replyOwnerId: Int?,
+    val replyPostId: Int?,
     val friendsOnly: Boolean = false,
-    val comments: Comments,
-    val likes: Int = 0,
+    val comments: Comments?,
+    val copyright: Copyright?,
+    val likes: Likes,
+    val reposts: Reposts?,
     val views: Int = 0,
+    val postType: String,
+    val postSource: String,
+    val attachments: Attachment?,
+    val geo: Geo?,
     val signerId: Int,
-    val canPin: Boolean= false,
-    val canDelete: Boolean= false,
-    val canEdit: Boolean= false,
-    val isPinned: Boolean= false,
-    val markedAsAds: Boolean= false,
-    val postponedId: Int,
- //   val attachments: Attachment
+    val copyHistory: String?,
+    val canPin: Boolean = false,
+    val canDelete: Boolean = false,
+    val canEdit: Boolean = false,
+    val isPinned: Boolean = false,
+    val markedAsAds: Boolean = false,
+    val isFavorite: Boolean = false,
+    val donut: Donut?,
+    val postponedId: Int?,
 )
 
-//interface Attachment{
-//    val url :String
-//}
-//
-//class PhotoAttachments() : Attachment{
-//override val url: String = ""
-//}
-//
-//class VideoAttachments() :Attachment{
-//    override val url: String = ""
-//}
-//
-//class AudioAttachments() :Attachment{
-//    override val url: String = ""
-//}
-//
-//class DocAttachments() :Attachment{
-//    override val url: String = ""
-//}
-//
-//class PostedPhotoAttachments() :Attachment{
-//    override val url: String = ""
-//}
+interface Attachment{
+    val url :String
+    val id :Int
+    val ownerId:Int
 
+}
 
+class PhotoAttachments(
+    override val url: String,
+    override val id: Int,
+    override val ownerId:Int,
+    val albumId: Int,
+    val updateUserId: Int
+) : Attachment
 
-//class Likes (
-//    val userLikes: Boolean,
-//    val canLike: Boolean,
-//    val canPublish: Boolean,
-//    count: Int = 0
-//) {
-//    var count = count
-//        set (value) {
-//            if (value < 0) {
-//                return
-//            }
-//            field = value
-//        }
-//}
+class VideoAttachments(override val url: String = "",
+        override val id: Int = 15,
+        override val ownerId:Int = 15) :Attachment
+
+class AudioAttachments(
+    override val url: String,
+    override val id: Int,
+    override val ownerId:Int,
+    val artist: String,
+    val title: String) :Attachment
+
+class DocAttachments(
+    override val url: String,
+    override val id: Int,
+    override val ownerId:Int
+) :Attachment
+
+class PostedPhotoAttachments(
+    override val url: String,
+    override val id: Int,
+    override val ownerId:Int
+) :Attachment
+
+class Likes (
+    val userLikes: Boolean,
+    val canLike: Boolean,
+    val canPublish: Boolean,
+    count: Int = 0
+) {
+    var count = count
+        set (value) {
+            if (value < 0) {
+                return
+            }
+            field = value
+        }
+}
+
+class Copyright (
+    val count: Int,
+    val canPost: Boolean,
+    val name: Boolean,
+    val type: Boolean,
+)
+
+class Reposts  (
+    val id: Int,
+    val user_reposted: Boolean,
+)
+
+class Donut (
+    val isDonut : Boolean,
+    val paidDuration: Int,
+    val placeholder : Any,
+    val canPublishFreeCopy : Boolean,
+    val editMode : String
+)
 
 class Comments (
     val count: Int,
@@ -70,6 +110,12 @@ class Comments (
     val groupsCanPost: Boolean,
     val canClose: Boolean,
     val canOpen: Boolean
+)
+
+class Geo  (
+    val type: String,
+    val coordinates: String,
+    val place : Boolean,
 )
 
 
